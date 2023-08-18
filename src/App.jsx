055f03e216014a48e3370d0bfd00c8fb5b23187b
abrahan <abrahandev@gmail.com>
2023-08-18 12:53:49 +0200

    Avatar agregado, App,jsx creado

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App () {
+    return (
+        <article>
+            <header>
+                <img src="/public/avatar.jpg" alt="Avatar de abrahan" />
+            </header>
+        </article>
+    )
+}
+
+export default App
