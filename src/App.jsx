7b9aee63fd1955574d720fedafe55704f5e5efde
abrahan <abrahandev@gmail.com>
2023-08-19 16:57:59 +0200

    Reutilizando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import { TwitterFollowCard } from "./TwitterFollowCard"
 
 function App () {
     return (
-      <TwitterFollowCard />
+      <div className="App"> 
+      <TwitterFollowCard userName="Abrahanfrias" name="Abrahan Frias" />
+      <TwitterFollowCard userName="Cristiano" name="CR7" />
+      <TwitterFollowCard userName="vinijr" name="Vinicius" />
+      </div>
     )
+  
 }
 
 export default App
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,11 +1,11 @@
-export function TwitterFollowCard () {
+export function TwitterFollowCard ({ userName, name, isFollowing}) {
     return (
         <article className="tw-followCard">
         <header className="tw-followCard-header">
-            <img className="tw-followCard-avatar" src="/public/avatar.jpg" alt="Avatar de abrahan" />
+            <img className="tw-followCard-avatar" src={`https://unavatar.io/twitter/${userName}`} alt="Avatar de abrahan" />
             <div className="tw-followCard-info">
-                <strong>Abrahan Frias</strong>
-                <span className="tw-followCard-infoUserName">@abrahanfrias</span>
+                <strong>{name}</strong>
+                <span className="tw-followCard-infoUserName">@{userName}</span>
             </div>
         </header>
 
