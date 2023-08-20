a205dcadca0847cf34f318110a8bf244e942d97e
abrahan <abrahandev@gmail.com>
2023-08-20 11:19:34 +0200

    Funciones como props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-// import './App.css'
+// import './App.css' se puede importar aqui directamente o en el main.jsx
 
 import { TwitterFollowCard } from "./TwitterFollowCard"
 
 function App () {
+  const formatUserName = (userName) => `@${userName}`
+
     return (
       <div className="App"> 
-      <TwitterFollowCard userName="Abrahanfrias" name="Abrahan Frias" />
-      <TwitterFollowCard userName="Cristiano" name="CR7" />
-      <TwitterFollowCard userName="vinijr" name="Vinicius" />
+      <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="Abrahanfrias" name="Abrahan Frias" />
+      <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} userName="Cristiano" name="CR7" />
+      <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="vinijr" name="Vinicius" />
       </div>
     )
   
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,11 +1,12 @@
-export function TwitterFollowCard ({ userName, name, isFollowing}) {
+export function TwitterFollowCard ({ formatUserName, userName, name, isFollowing}) {
+    console.log(isFollowing)
     return (
         <article className="tw-followCard">
         <header className="tw-followCard-header">
             <img className="tw-followCard-avatar" src={`https://unavatar.io/twitter/${userName}`} alt="Avatar de abrahan" />
             <div className="tw-followCard-info">
                 <strong>{name}</strong>
-                <span className="tw-followCard-infoUserName">@{userName}</span>
+                <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
             </div>
         </header>
 
